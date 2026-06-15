25af533f8071f467d8631f8a49129fb0a629dc5b
Ivan Boytsov <ivanboitsov386@gmail.com>
2026-06-16 02:30:57 +0300

    feat: add 2-nd lab report

--- lab-02/src/stribog.py
+++ lab-02/src/stribog.py
@@ -10,6 +10,3 @@ digest = hasher.digest()
 print("Полный хэш:", binascii.hexlify(digest).decode())
 variant_number = digest[-1]
 print("Номер варианта (от 0 до 255):", variant_number)
-
-# Полный хэш: 0e664ff746caf1eedce31026317de06880031b7230571aaf0a2f3fbbe86b9f98
-# Номер варианта (от 0 до 255): 152
